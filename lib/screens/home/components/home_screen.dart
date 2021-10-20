7317c553dadb42f066317e24b46ef448189a8add
A <solardayz@naver.com>
2021-10-20 17:02:54 +0900

    리커맨트 부분 개발 중인데 어려움

--- lib/screens/home/components/home_screen.dart
+++ lib/screens/home/components/home_screen.dart
@@ -18,54 +18,100 @@ class HomeScreen extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Header(size, context),
-          titileWithMoreButton(),
+          titleWithMoreButton(),
+          Container(
+            child: Column(
+              children: [
+                Image.asset('assets/images/image_1.png'),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(10),
+                      bottomRight: Radius.circular(10),
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        offset: Offset(0, 10),
+                        blurRadius: 50,
+                        color: kPrimaryColor.withOpacity(0.23),
+                      ),
+                    ],
+                  ),
+                  child: Row(
+                    children: [
+                      RichText(
+                        text: TextSpan(
+                          children: [
+                            TextSpan(
+                                text: "Samantha".toUpperCase(),
+                                style: Theme.of(context).textTheme.button),
+                            TextSpan(
+                                text: "Russia".toUpperCase(),
+                                style: Theme.of(context).textTheme.button),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
   }
 
-  Row titileWithMoreButton() {
-    return Row(
-      children: [
-        Container(
-          height: 24,
-          child: Stack(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(left: kDefaultPadding / 4),
-                child: Text(
-                  'Recomended',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
+  Padding titleWithMoreButton() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+      child: Row(
+        children: [
+          Container(
+            height: 24,
+            child: Stack(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: kDefaultPadding / 4),
+                  child: Text(
+                    'Recomended',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-              ),
-              Positioned(
-                bottom: 0,
-                left: 0,
-                right: 0,
-                child: Container(
-                  margin: EdgeInsets.only(right: kDefaultPadding / 4),
-                  height: 7,
-                  color: kPrimaryColor.withOpacity(0.2),
-                ),
-              )
-            ],
-          ),
-        ),
-        Spacer(),
-        TextButton(
-          onPressed: () {},
-          child: Text(
-            'More',
-            style: TextStyle(
-              color: Colors.white,
-              backgroundColor: kPrimaryColor,
+                Positioned(
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                  child: Container(
+                    margin: EdgeInsets.only(right: kDefaultPadding / 4),
+                    height: 7,
+                    color: kPrimaryColor.withOpacity(0.2),
+                  ),
+                )
+              ],
             ),
           ),
-        ),
-      ],
+          Spacer(),
+          ElevatedButton(
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(kPrimaryColor),
+            ),
+            onPressed: () {
+              print('More....');
+            },
+            child: Text(
+              'More',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 
