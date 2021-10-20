59001b85a8a9aa0102cdb9d6798fea84232d0846
A <solardayz@naver.com>
2021-10-20 10:20:32 +0900

    플랜트 앱 따라하기 기초 세팅 완료

---
+++ android/app/src/main/kotlin/com/clc/flutter_plants/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.clc.flutter_plants
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_plants/constants.dart';
+import 'package:flutter_plants/screens/home/components/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Plant App',
+      theme: ThemeData(
+        scaffoldBackgroundColor: kBackgroundColor,
+        primaryColor: kPrimaryColor,
+        textTheme: Theme.of(context).textTheme.apply(bodyColor: kTextColor),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
