e12695f1e4226d571d659d14a7078802af20a1b0
A <solardayz@naver.com>
2021-10-20 13:45:05 +0900

    이미지 아이콘  추가 및 해더 코딩

--- lib/screens/home/components/home_screen.dart
+++ lib/screens/home/components/home_screen.dart
@@ -1,10 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_plants/constants.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    Size size = MediaQuery.of(context).size;
+
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Header(size, context),
+        ],
+      ),
+    );
+  }
+
+  Widget Header(Size size, BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: kDefaultPadding * 2.5),
+      height: size.height * 0.2,
+      child: Container(
+        padding: EdgeInsets.only(
+          left: kDefaultPadding,
+          right: kDefaultPadding,
+          bottom: kDefaultPadding,
+        ),
+        height: size.height * 0.2,
+        decoration: BoxDecoration(
+          color: kPrimaryColor,
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(36),
+            bottomRight: Radius.circular(36),
+          ),
+        ),
+        child: Row(
+          children: [
+            Text(
+              'Hi Uishopy!',
+              style: Theme.of(context)
+                  .textTheme
+                  .headline5!
+                  .copyWith(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+            // Image.asset("assets/images/logo.png")
+          ],
+        ),
+      ),
+    );
   }
 }
